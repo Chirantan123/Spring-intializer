352b86837dbc6d2d5200b7ff000e5241f05189ca
Chirantan123 <chirantanism@gmail.com>
2021-02-22 11:30:13 +0530

    added spring-web deps

--- src/main/java/com/example/Spring/Session1/SpringSession1Application.java
+++ src/main/java/com/example/Spring/Session1/SpringSession1Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSession1Application {
-
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		SpringApplication.run(SpringSession1Application.class, args);
 	}
 
