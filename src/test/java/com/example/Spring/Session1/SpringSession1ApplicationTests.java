26d5de7cc2b933dbe87fe5d6fa4ce96a55261744
Chirantan123 <chirantanism@gmail.com>
2021-02-22 10:59:44 +0530

    First commit on spring

---
+++ src/test/java/com/example/Spring/Session1/SpringSession1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Session1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSession1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
